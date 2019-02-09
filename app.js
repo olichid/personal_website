8b9ee16de22e7ed67a131df66eca0173bf5cbb60
olichid <zhengchengc@student.unimelb.edu.au>
2019-02-10 00:36:18 +1100

    basic frame

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 let express = require("express"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
     app = express(),
     port = 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
 
 app.get("/", function (req, res) {
-    console.log("Aha")
+    res.render("frontpage");
+});
+
+app.get("/blogs", function (req, res) {
+    res.render("blogs");
+});
+
+app.get("/gears", function (req, res) {
+    res.render("gears");
+});
+
+app.get("/contact", function (req, res) {
+    res.render("contact");
 });
 
 app.listen(port, function () {
